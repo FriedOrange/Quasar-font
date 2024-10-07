ea83e2c91a8ca6cbf57c8f36d765a1c109ecfe69
Brad Neil <brad@friedorange.xyz>
2024-10-07 21:40:00 +1100

    Merge shared glyphs into Regular style

--- build.bat
+++ build.bat
@@ -5,11 +5,15 @@ sfd2ufo source\QuasarClassic-MASTER-900.sfd source\QuasarClassic-MASTER-900.ufo
 copy source\features-100.fea source\QuasarClassic-MASTER-100.ufo\features.fea
 copy source\features-900.fea source\QuasarClassic-MASTER-900.ufo\features.fea
 
-sfd2ufo source\Quasar-MASTER-100.sfd source\Quasar-MASTER-100.ufo
-sfd2ufo source\Quasar-MASTER-600.sfd source\Quasar-MASTER-600.ufo
-sfd2ufo source\Quasar-MASTER-900.sfd source\Quasar-MASTER-900.ufo
-@REM copy source\features-100.fea source\Quasar-MASTER-100.ufo\features.fea
-@REM copy source\features-900.fea source\Quasar-MASTER-900.ufo\features.fea
+fontforge -c "open('source\\Quasar-MASTER-100.sfd').mergeFonts('source\\QuasarClassic-MASTER-100.sfd').save('source\\temp.sfd')"
+sfd2ufo source\temp.sfd source\Quasar-MASTER-100.ufo
+fontforge -c "open('source\\Quasar-MASTER-600.sfd').mergeFonts('source\\QuasarClassic-MASTER-600.sfd').save('source\\temp.sfd')"
+sfd2ufo source\temp.sfd source\Quasar-MASTER-600.ufo
+fontforge -c "open('source\\Quasar-MASTER-900.sfd').mergeFonts('source\\QuasarClassic-MASTER-900.sfd').save('source\\temp.sfd')"
+sfd2ufo source\temp.sfd source\Quasar-MASTER-900.ufo
+del source\temp.sfd
+copy source\features-100.fea source\Quasar-MASTER-100.ufo\features.fea
+copy source\features-900.fea source\Quasar-MASTER-900.ufo\features.fea
 
 @rem build OpenType fonts
 @cd source
